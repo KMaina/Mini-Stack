469d2409e6aff6b17d7204aa40e44c79ebd4b7f0
Clint <cmaruti93@gmail.com>
2018-10-01 14:44:18 +0300

    [Feature #160885529] Adds add and Delete Function

--- Mini Stack/__init__.py
+++ Mini Stack/__init__.py
@@ -1,13 +1,36 @@
-class comment(object):
+from datetime import datetime
+
+class Comment(object):
     """This class holds methods for adding, 
     modifying, deleting and replying to a comment
     """
     def __init__(self):
         self.comment = []
 
-    def delete(self,name,comment):
+    def add(self,name,comment):
         """
-        This funtion deletes a comment from the list
+        This funtion adds a comment and name to the list
         """
-        self.dictionary{}
-        
+        holder = {}
+
+        holder['name'] = name
+        holder['comment'] = comment
+        holder['id'] = len(self.comment)+1
+        holder['Time'] = datetime.datetime.now()
+        self.comment.append(holder)
+        return self.comment
+
+    def delete(self,name):
+        """
+        This function deletes comment from the list
+        """    
+        for id in self.comment:
+            if id in id['id']:
+                del(self.comment['comment'])
+                return self.comment
+
+if __name__ == '__main__':
+    a = Comment().add("Clint","My name is Clint")
+    d = Comment().add("Ken","This is my comment")
+    b = Comment().delete("Clint")
+    print (d)
