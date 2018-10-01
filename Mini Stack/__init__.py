4fe2f802dd719869f1d2dce02e39cf163ecc48f7
Clint <cmaruti93@gmail.com>
2018-10-01 13:47:49 +0300

    [Feature #160885529] Adds Delete Fucntion

---
+++ Mini Stack/__init__.py
@@ -0,0 +1,13 @@
+class comment(object):
+    """This class holds methods for adding, 
+    modifying, deleting and replying to a comment
+    """
+    def __init__(self):
+        self.comment = []
+
+    def delete(self,name,comment):
+        """
+        This funtion deletes a comment from the list
+        """
+        self.dictionary{}
+        
